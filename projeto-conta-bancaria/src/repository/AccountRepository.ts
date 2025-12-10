17a0e0dfd7b578b3c9caccbf4459d5cfd8bf4596
Matheus Alan <matheusalanpv@outlook.com>
2025-12-10 15:06:17 -0300

    implementacao da interface AccountRepository

---
+++ projeto-conta-bancaria/src/repository/AccountRepository.ts
@@ -0,0 +1,15 @@
+import { Account } from "../model/Account";
+
+export interface AccountRepository {
+
+    findByNumber(num: number): void;
+    listAll(): void;
+    save(account: Account): void;
+    update(account: Account): void;
+    delete(num: number): void;
+
+    withdraw(accountNumber: number, value: number): void;
+    deposit(accountNumber: number, value: number): void;
+    transfer(originNumber: number, destinationNumber: number, value: number): void;
+
+}
